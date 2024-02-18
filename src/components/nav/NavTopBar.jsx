8ff691cd85990b016f42e8524d783524b233ab35
Michael Cortez <CortezDevelopement@gmail.com>
2024-02-18 12:39:21 -0700

    made basic layout and styles for top nav bar

--- src/components/nav/NavTopBar.jsx
+++ src/components/nav/NavTopBar.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 const NavTopBar = () => {
   return (
-    <div>
-      <div>Nav</div>
-      <div>Top Bar</div>
+    <div className="nav-top-bar-wrapper">
+      <div className="nav-top-bar-logo-links-wrapper">
+        <div className="nav-top-bar-logo">
+          PixelPulse
+        </div>
+
+        <div className="nav-top-bar-links-wrapper">
+          <div className="nav-top-bar-link-wrapper">Home</div>
+          <div className="nav-top-bar-link-wrapper">About Us</div>
+          <div className="nav-top-bar-link-wrapper">Services</div>
+          <div className="nav-top-bar-link-wrapper">Contact</div>
+        </div>
+      </div>
+
+      <div className="hamburger-wrapper">
+        <div className="bar arrow-top"></div>
+        <div className="bar arrow-middle"></div>
+        <div className="bar arrow-bottom"></div>
+      </div>
     </div>
   )
 }
--- src/master/App.js
+++ src/master/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../components/home/Home";
-
 import About from "../components/about/About";
 import Services from "../components/services/Services";
 import Contact from "../components/contact/Contact";
+import NavTopBar from "../components/nav/NavTopBar";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavTopBar />
+
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about-us" element={<About />} />
