8b457e6218982d876ce813e771fdfdbe0c9c88f1
Michael Cortez <CortezDevelopement@gmail.com>
2024-02-19 21:57:37 -0700

    finished basic layout and styles for about us section on homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -34,6 +34,124 @@ const Home = () => {
           />
         </div>
       </div>
+
+      <div className="homepage-about-us-wrapper">
+        <div className="homepage-about-us-image-wrapper">
+          <img className="homepage-about-us-image" src="https://source.unsplash.com/random/350x350" />
+        </div>
+
+        <div className="homepage-about-us-text-wrapper">
+          <div className="homepage-about-us-header">
+            Who We Are
+          </div>
+
+          <div className="homepage-about-us-sub-header">
+            We Are <span className="homepage-text-varient">All In One</span> IT Solution & Technology Company
+          </div>
+
+          <div className="homepage-about-us-paragraphs-wrapper">
+            <p className="homepage-about-us-text">
+              At PixelPulse, we are a dynamic and innovative IT services company specializing in developing custom software solutions to address the unique and evolving needs of businesses.
+            </p>
+
+            <p className="homepage-about-us-text">
+              With our extensive expertise and passion for technology, we empower organizations to achieve their goals and unlock their full potential through tailored web applications, mobile apps, enterprise software, and seamless integrations with existing systems.
+            </p>
+          </div>
+
+          <button className="homepage-about-us-action-button">
+            Learn More About Us
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          Services
+        </div>
+
+        <div>We Provide the Best Quality <span>Services</span></div>
+
+        <div>
+          From developing custom web app solutions to designing seamless user experiences, we have the know-how to tackle any challenge.
+        </div>
+
+        <button>
+          Explore All Services
+        </button>
+
+        <div>
+          <div>
+            <div>
+              <img />
+            </div>
+
+            <div>
+              Web Design
+            </div>
+
+            <div>
+              We create customized software solutions that run specifically on mobile devices.
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <img />
+            </div>
+
+            <div>
+              Web Development
+            </div>
+
+            <div>
+              We create customized software solutions that run specifically on mobile devices.
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <img />
+            </div>
+
+            <div>
+              Mobile App Development
+            </div>
+
+            <div>
+              We create customized software solutions that run specifically on mobile devices.
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <img />
+            </div>
+
+            <div>
+              Search Engine Optimization
+            </div>
+
+            <div>
+              We create customized software solutions that run specifically on mobile devices.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          Why Choose Us
+        </div>
+
+        <div>
+          We Help You Increase Your Sales Through High Quality Solutions
+        </div>
+
+        <div>
+          When it comes to meeting your IT needs and achieving your business goals, choosing the right partner is crucial. At PixelPulse, we are dedicated to providing exceptional IT services that empower your business to thrive in the digital landscape.
+        </div>
+      </div>
     </div>
   )
 }
