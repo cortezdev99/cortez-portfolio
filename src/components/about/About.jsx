6eae8e6e9253fda0b1bd97387f8e7c714984df5b
Michael Cortez <CortezDevelopement@gmail.com>
2024-02-27 22:25:58 -0700

    finished basic layout and styles for our process section on about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,9 +2,133 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <div>About</div>
-      <div>page</div>
+    <div className="about-us-wrapper">
+      <div className="about-us-section-image-wrapper">
+        <div className="about-us-section-image" />
+        <div className="about-us-section-image-overlay-text">lorem ipsum dollar siet</div>
+      </div>
+
+      <div className="about-us-intro-section-wrapper">
+        <div className="about-us-intro-image-wrapper">
+          <img className="about-us-intro-image" src="https://source.unsplash.com/random/350x350" />
+        </div>
+
+        <div className="about-us-intro-wrapper">
+          <div className="about-us-intro-header">
+            Who We Are
+          </div>
+
+          <div className="about-us-intro-subheader">
+            360* business consulting
+          </div>
+
+          <div className="about-us-intro-text">
+            whether your company looks for a financial consulting, investment risks assessments or an interim, HR managment, we're ready to provide that for you.
+          </div>
+
+          <button className="about-us-intro-action-button">
+            Contact Us
+          </button>
+        </div>
+      </div>
+
+      <div className="about-us-opening-statements-wrapper">
+        <div className="about-us-opening-statement-wrapper">
+          <div className="about-us-opening-statement-header">
+            Our Vision
+          </div>
+
+          <div className="about-us-opening-statement-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laboriosam delectus nisi autem recusandae, corporis hic ducimus sit inventore unde maiores aliquam perferendis eligendi exercitationem sequi itaque beatae perspiciatis dolores.
+            Deserunt ipsa eveniet commodi a excepturi harum, officia recusandae minus alias illo tempora delectus possimus nostrum at quos dolores odio est iusto quas ut doloremque, blanditiis nobis provident? Culpa, rerum!
+          </div>
+        </div>
+
+        <div className="about-us-opening-statement-wrapper">
+          <div className="about-us-opening-statement-header">
+            Our Mission
+          </div>
+
+          <div className="about-us-opening-statement-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laboriosam delectus nisi autem recusandae, corporis hic ducimus sit inventore unde maiores aliquam perferendis eligendi exercitationem sequi itaque beatae perspiciatis dolores.
+            Deserunt ipsa eveniet commodi a excepturi harum, officia recusandae minus alias illo tempora delectus possimus nostrum at quos dolores odio est iusto quas ut doloremque, blanditiis nobis provident? Culpa, rerum!
+          </div>
+        </div>
+      </div>
+
+      <div className="about-us-our-process-container">
+        <div className="about-us-our-process-overview-wrapper">
+          <div className="about-us-our-process-overview-header">
+            Our Process
+          </div>
+
+          <div className="about-us-our-process-overview-subheader">
+            We Follow Four Simple Steps
+          </div>
+
+          <div className="about-us-our-process-overview-text">
+            With our expertise in providing cutting-edge technology solutions, PixelPulse employs a straightforward yet powerful approach to ensure our success.
+          </div>
+        </div>
+
+        <div className="about-us-our-process-steps-wrapper">
+          <div className="about-us-our-process-step-wrapper">
+            <div className="about-us-our-process-step-num">
+              Step 1
+            </div>
+
+            <div className="about-us-our-process-step-header">
+              Discuss
+            </div>
+
+            <div className="about-us-our-process-step-description">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem impedit possimus eius dolor dignissimos, quod ad necessitatibus iste neque fuga consequatur voluptas assumenda cum hic, quos asperiores architecto, consectetur aut.
+            </div>
+          </div>
+
+          <div className="about-us-our-process-step-wrapper">
+            <div className="about-us-our-process-step-num">
+              Step 2
+            </div>
+
+            <div className="about-us-our-process-step-header">
+              Refine
+            </div>
+
+            <div className="about-us-our-process-step-description">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem impedit possimus eius dolor dignissimos, quod ad necessitatibus iste neque fuga consequatur voluptas assumenda cum hic, quos asperiores architecto, consectetur aut.
+            </div>
+          </div>
+
+          <div className="about-us-our-process-step-wrapper">
+            <div className="about-us-our-process-step-num">
+              Step 3
+            </div>
+
+            <div className="about-us-our-process-step-header">
+              Design
+            </div>
+
+            <div className="about-us-our-process-step-description">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem impedit possimus eius dolor dignissimos, quod ad necessitatibus iste neque fuga consequatur voluptas assumenda cum hic, quos asperiores architecto, consectetur aut.
+            </div>
+          </div>
+
+          <div className="about-us-our-process-step-wrapper">
+            <div className="about-us-our-process-step-num">
+              Step 4
+            </div>
+
+            <div className="about-us-our-process-step-header">
+              Execute
+            </div>
+
+            <div className="about-us-our-process-step-description">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem impedit possimus eius dolor dignissimos, quod ad necessitatibus iste neque fuga consequatur voluptas assumenda cum hic, quos asperiores architecto, consectetur aut.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
